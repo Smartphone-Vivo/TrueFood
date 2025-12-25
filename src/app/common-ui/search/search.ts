cec19c7bd584f99343025a25e279f631bad49047
Smartphone-Vivo <aleksejpopov268@gmail.com>
2025-12-25 21:03:15 +0300

    Поиск

---
+++ src/app/common-ui/search/search.ts
@@ -0,0 +1,27 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {TuiButton, TuiTextfield} from '@taiga-ui/core';
+import {FormsModule} from '@angular/forms';
+import {TuiChevron, TuiTextarea} from '@taiga-ui/kit';
+
+@Component({
+  selector: 'app-search',
+  imports: [
+    TuiTextfield,
+    FormsModule,
+    TuiTextarea,
+    TuiButton,
+    TuiChevron
+  ],
+  templateUrl: './search.html',
+  styleUrl: './search.scss',
+})
+export class Search {
+
+  @Output() searchValue = new EventEmitter<string>()
+
+  value: string = ''
+
+  search(){
+    this.searchValue.emit(this.value)
+  }
+}
